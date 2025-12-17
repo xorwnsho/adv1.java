6a22b55f0017252c1ecbfd7e5bfd0c4b5144f0a0
OhJunTaekk <mt3026super@naver.com>
2025-12-17 21:33:37 +0900

    스레드 제어와 생명주기 2

---
+++ src/main/java/thread/control/printer/MyPrinterV1.java
@@ -0,0 +1,52 @@
+package thread.control.printer;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static thread.util.MyLogger.log;
+import static thread.util.ThreadUtils.sleep;
+
+public class MyPrinterV1 {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Thread printerThread = new Thread(printer, "printer");
+        printerThread.start();
+
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            log("프린터할 문서를 입력하세요. 종료(q) : ");
+            String input = scanner.nextLine();
+            if(input.equals("q")){
+                printer.work = false;
+                break;
+            }
+            printer.addJob(input);
+        }
+    }
+
+    static class Printer implements Runnable {
+        volatile boolean work = true;
+
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+        @Override
+        public void run() {
+            while(work){
+                if(jobQueue.isEmpty()){
+                    continue;
+                }
+
+                String job = jobQueue.poll();
+                log("출력 시작: " + job + ", 대기 문서: " + jobQueue);
+                sleep(1000);
+                log("출력 완료: " + job);
+            }
+            log("프린터 종료");
+        }
+
+        public void addJob(String job){
+            jobQueue.offer(job);
+        }
+    }
+}
